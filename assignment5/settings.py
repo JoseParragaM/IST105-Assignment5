5b5d35bd0bd146e7fd38346e1b8dd33cd760e58a
JoseParragaM <jgparraga99@gmail.com>
2025-06-09 17:17:43 -0500

    First commit in feature1

--- assignment5/settings.py
+++ assignment5/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-an@i*cy)ami*v@9lcv@8y*o(kh42d*+jxho-z%s7u-fo0os%5g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'puzzle',
 ]
 
 MIDDLEWARE = [
--- puzzle/views.py
+++ puzzle/views.py
@@ -1,3 +1,54 @@
+import random
 from django.shortcuts import render
+from .forms import PuzzleForm
 
-# Create your views here.
+def puzzle_view(request):
+    first_puzzle = []
+    second_puzzle = []
+    third_puzzle = []
+    form = PuzzleForm(request.POST or None)
+
+    if request.method == 'POST' and form.is_valid():
+        number = form.cleaned_data['number']
+        text = form.cleaned_data['text']
+
+        if number % 2 == 0:
+            number_sqrt = number ** (1/2)
+            first_puzzle.append(f"The number {number} is even. Its square root is {number_sqrt}.")
+        else:
+            number_cube = number ** 3
+            first_puzzle.append(f"The number {number} is odd. Its cube is {number_cube}.")
+
+        text_binary = ' '.join(format(ord(i), '08b') for i in text)
+        text_vowels = sum(i.lower() in 'aeiou' for i in text)
+        second_puzzle.append(f"Binary: {text_binary}")
+        second_puzzle.append(f"Vowel Count: {text_vowels}")
+
+        random_number = random.randint(1, 100)
+        third_puzzle.append(f"The secret number is {random_number}.")
+        guesses = []
+        correct = False
+
+        for i in range(1,6):
+            guess = random.randint(1, 100)
+            guesses.append(guess)
+            if guess == random_number:
+                third_puzzle.append(f"Attempt {i}: {guess} (Correct!)")
+                correct = True
+                break
+            elif guess > random_number:
+                third_puzzle.append(f"Attempt {i}: {guess} (Too high!)")
+            else:
+                third_puzzle.append(f"Attempt {i}: {guess} (Too low!)")
+        else:
+            third_puzzle.append("You did not find the treasure in 5 attempts.")
+
+        if correct:
+            third_puzzle.append(f"You found the treasure in {len(guesses)} attempts!")
+
+    return render(request, 'puzzle/form.html', {
+        'form': form,
+        'first_puzzle': first_puzzle,
+        'second_puzzle':   second_puzzle,
+        'third_puzzle':   third_puzzle,
+    })
